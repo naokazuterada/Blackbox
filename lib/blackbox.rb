e5bd500da8c354c50e00e45495545f59e6298474
Naokazu Terada <naokazu.terada@gmail.com>
2017-08-31 17:06:25 +0900

    - Add rubocop settings - Rubocop auto-correct

--- lib/blackbox.rb
+++ lib/blackbox.rb
@@ -1,6 +1,8 @@
-require "blackbox/version"
-require "blackbox/cli"
+# frozen_string_literal: true
+require 'blackbox/version'
+require 'blackbox/cli'
 
+# This is CLI tool for Blackbox game
 module Blackbox
   # Your code goes here...
 end
--- lib/blackbox/cli.rb
+++ lib/blackbox/cli.rb
@@ -1,3 +1,4 @@
+# frozen_string_literal: true
 require 'blackbox'
 require 'thor'
 require 'yaml'
